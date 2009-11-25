24c67079987cf41a3661d5929ae36c425b4dde95
Prajwala <azriprajwala@43559.no-reply.drupal.org>
2009-11-25 19:33:48 +0000

    updated head with the 6 branch changes

--- accordion_init.js
+++ accordion_init.js
@@ -1,34 +1,6 @@
 // $Id$
 Drupal.behaviors.accordion_blocks = function(){
-    // left side blocks
-    var blocks;
-    if($('#sidebar-left').length > 0){
-      blocks = $("[id^='block']",$('#sidebar-left'));
-    }
-    else {
-      blocks = $("[id^='block']",$('.sidebar-left'));
-    }
-    var containerLeft = $('<div></div>').prependTo(blocks.parent());
-    containerLeft.append(blocks);
-    containerLeft.accordion({ header: "h2", autoHeight: false });
-
-    // right side blocks
-    if($('#sidebar-right').length > 0) {
-      blocks = $("[id^='block']",$('#sidebar-right'));
-    }
-    else { 
-      blocks = $("[id^='block']",$('.sidebar-right'));
-    }	
-    var containerRight = $('<div></div>').prependTo(blocks.parent());
-    containerRight.append(blocks);
-    containerRight.accordion({ header: "h2", autoHeight: false });
-
-    // just single navigation
-    var blocks = $("[id^='block']",$('.side_nav'));
-    if(blocks){
-      var containerNav = $('<div></div>').prependTo(blocks.parent());
-      containerNav.append(blocks);
-      containerNav.accordion({ header: "h2", autoHeight: false });
-    }
+    $('#accordion_blocks_left').accordion({header: "h2", autoHeight: false});
+    $('#accordion_blocks_right').accordion({header: "h2", autoHeight: false});
 };
 

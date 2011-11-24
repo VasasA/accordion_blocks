a8f88c6e70edce2362d3b598ca7f89833588f7f5
Prajwala <m.prajwala@gmail.com>
2011-11-24 19:10:10 +0530

    Changed accordion blocks feature to apply the effect without changing the region template

--- accordion_init.js
+++ accordion_init.js
@@ -1,9 +1,9 @@
 (function($) {
-    Drupal.behaviors.accordion_blocks = {
-	attach: function() {
+  Drupal.behaviors.accordion_blocks = {
+	  attach: function() {
 	    $('.accordion_blocks_container').accordion({header: "h2", autoHeight: false});
-	}
-    };
+	  }
+  };
 })(jQuery);
 
 
--- block.tpl.php
+++ block.tpl.php
@@ -1,12 +1,7 @@
-<?php
-?>
 <div id="block-<?php print $block->module . '-' . $block->delta; ?>" <?php print $attributes; ?>>
-
-
-<?php if (!empty($block->subject)): ?>
+  <?php if (!empty($block->subject)): ?>
     <h2 <?php print $title_attributes; ?>><a href="#"><?php print $block->subject ?></a></h2>
-    <?php endif;?>
-
-    <div <?php print $content_attributes; ?>><?php print $content ?></div>
+  <?php endif;?>
+  <div <?php print $content_attributes; ?>><?php print $content ?></div>
 </div>
 
